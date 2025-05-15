7c30068688e5c8f05177645650773836bb696a1a
Administrator <Administrator@DESKTOP-LM8UD0R>
2025-05-15 19:29:14 +0700

    1 commit

--- src/main/java/com/example/jpa/beforejpa/BookDAO.java
+++ src/main/java/com/example/jpa/beforejpa/BookDAO.java
@@ -15,7 +15,7 @@ public class BookDAO {
         int so =0;
         String sql =  "INSERT INTO `students`.`book` (`isbn`, `title`) VALUES (?, ?)";  
         try{
-            Connection cn = db.getConnection();// UPDATE HÊRE
+            Connection cn = db.getConnection();// UPDATE HÊRE UPDATE AGAIN
             PreparedStatement  pre = cn.prepareStatement(sql);
             pre.setString(1, b.getIsbn());
             pre.setString(2, b.getTitle());
